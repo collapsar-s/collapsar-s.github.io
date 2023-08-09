33b5ca2f1dbf7d2762ca6105b9fe8bd76adacf8a
collapsars <collapsars@foxmail.com>
2023-08-09 22:19:05 +0800

    Site updated: 2023-08-09 22:19:05

---
+++ js/cat.js
@@ -0,0 +1,147 @@
+if (document.body.clientWidth > 992) {
+    function getBasicInfo() {
+        /* ���ڸ߶� */
+        var ViewH = $(window).height();
+        /* document�߶� */
+        var DocH = $("body")[0].scrollHeight;
+        /* �����ĸ߶� */
+        var ScrollTop = $(window).scrollTop();
+        /* �ɹ����ĸ߶� */
+        var S_V = DocH - ViewH;
+        var Band_H = ScrollTop / (DocH - ViewH) * 100;
+        return {
+            ViewH: ViewH,
+            DocH: DocH,
+            ScrollTop: ScrollTop,
+            Band_H: Band_H,
+            S_V: S_V
+        }
+    };
+    function show(basicInfo) {
+        if (basicInfo.ScrollTop > 0.001) {
+            $(".neko").css('display', 'block');
+        } else {
+            $(".neko").css('display', 'none');
+        }
+    }
+    (function ($) {
+        $.fn.nekoScroll = function (option) {
+            var defaultSetting = {
+                top: '0',
+                scroWidth: 6 + 'px',
+                z_index: 9999,
+                zoom: 0.9,
+                borderRadius: 5 + 'px',
+                right: 60 + 'px',
+                // ������Ի�Ϊ��ϲ����ͼƬ�������Ҿͻ�Ϊ��ѩ�ˣ�����Ҫ��ͼ
+                nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
+                hoverMsg: "������~",
+                color: "#6f42c1",
+                during: 500,
+                blog_body: "body",
+            };
+            var setting = $.extend(defaultSetting, option);
+            var getThis = this.prop("className") !== "" ? "." + this.prop("className") : this.prop("id") !== "" ? "#" +
+                this.prop("id") : this.prop("nodeName");
+            if ($(".neko").length == 0) {
+                this.after("<div class=\"neko\" id=" + setting.nekoname + " data-msg=\"" + setting.hoverMsg + "\"></div>");
+            }
+            let basicInfo = getBasicInfo();
+            $(getThis)
+                .css({
+                    'position': 'fixed',
+                    'width': setting.scroWidth,
+                    'top': setting.top,
+                    'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+                    'z-index': setting.z_index,
+                    'background-color': setting.bgcolor,
+                    "border-radius": setting.borderRadius,
+                    'right': setting.right,
+                    'background-image': 'url(' + setting.scImg + ')',
+                    'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+                    'background-size': 'contain'
+                });
+            $("#" + setting.nekoname)
+                .css({
+                    'position': 'fixed',
+                    'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+                    'z-index': setting.z_index * 10,
+                    'right': setting.right,
+                    'background-image': 'url(' + setting.nekoImg + ')',
+                });
+            show(getBasicInfo());
+            $(window)
+                .scroll(function () {
+                    let basicInfo = getBasicInfo();
+                    show(basicInfo);
+                    $(getThis)
+                        .css({
+                            'position': 'fixed',
+                            'width': setting.scroWidth,
+                            'top': setting.top,
+                            'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+                            'z-index': setting.z_index,
+                            'background-color': setting.bgcolor,
+                            "border-radius": setting.borderRadius,
+                            'right': setting.right,
+                            'background-image': 'url(' + setting.scImg + ')',
+                            'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+                            'background-size': 'contain'
+                        });
+                    $("#" + setting.nekoname)
+                        .css({
+                            'position': 'fixed',
+                            'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+                            'z-index': setting.z_index * 10,
+                            'right': setting.right,
+                            'background-image': 'url(' + setting.nekoImg + ')',
+                        });
+                    if (basicInfo.ScrollTop == basicInfo.S_V) {
+                        $("#" + setting.nekoname)
+                            .addClass("showMsg")
+                    } else {
+                        $("#" + setting.nekoname)
+                            .removeClass("showMsg");
+                        $("#" + setting.nekoname)
+                            .attr("data-msg", setting.hoverMsg);
+                    }
+                });
+            this.click(function (e) {
+                btf.scrollToDest(0, 500)
+            });
+            $("#" + setting.nekoname)
+                .click(function () {
+                    btf.scrollToDest(0, 500)
+                });
+            return this;
+        }
+    })(jQuery);
+
+    $(document).ready(function () {
+        //�����Զ���
+        $("#myscoll").nekoScroll({
+            bgcolor: 'rgb(0 0 0 / .5)', //������ɫ��û�����ӱ���ͼƬʱ��Ч
+            borderRadius: '2em',
+            zoom: 0.9
+        }
+        );
+        //�Զ��壨ȥ������ע�ͣ���ע�͵������Ĳ鿴Ч����
+        /*
+        $("#myscoll").nekoScroll({
+            nekoname:'neko1', //nekoname���൱��id
+            nekoImg:'img/è��.png', //neko�ı���ͼƬ
+            scImg:"img/��1.png", //���ӵı���ͼƬ
+            bgcolor:'#1e90ff', //������ɫ��û�����ӱ���ͼƬʱ��Ч
+            zoom:0.9, //���ӳ��ȵ�����ֵ
+            hoverMsg:'���~��', //��긡����neko�Ϸ��ĶԻ�����Ϣ
+            right:'100px', //����ҳ���ұߵľ���
+            fontFamily:'����', //�Ի�������
+            fontSize:'14px', //�Ի�������Ĵ�С
+            color:'#1e90ff', //�Ի���������ɫ
+            scroWidth:'8px', //���ӵĿ���
+            z_index:100, //���ý����˰�
+            during:1200, //�Ӷ������ײ�������ʱ��
+        });
+        */
+    })
+}
